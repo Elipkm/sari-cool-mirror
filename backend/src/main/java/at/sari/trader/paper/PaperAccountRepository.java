0b4ccb7f9c49b240a6a63df8a208aa69d3544d9a
Elipkm <57539765+Elipkm@users.noreply.github.com>
2026-08-29 21:41:19 +0200

    Milestone 2: add paper account repository

---
+++ backend/src/main/java/at/sari/trader/paper/PaperAccountRepository.java
@@ -0,0 +1,5 @@
+package at.sari.trader.paper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaperAccountRepository extends JpaRepository<PaperAccount, Long> {}
