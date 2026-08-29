afa790868c3730b771c0bcb60f14201160e5a6ae
Elipkm <57539765+Elipkm@users.noreply.github.com>
2026-08-29 21:41:26 +0200

    Milestone 2: add paper position repository

---
+++ backend/src/main/java/at/sari/trader/paper/PaperPositionRepository.java
@@ -0,0 +1,5 @@
+package at.sari.trader.paper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaperPositionRepository extends JpaRepository<PaperPosition, String> {}
